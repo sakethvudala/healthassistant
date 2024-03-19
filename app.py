0e9ebb6d8ba2ebab4a7567c8cf49d1e58b2ce89e
sakethvudala <154961250+sakethvudala@users.noreply.github.com>
2024-03-19 18:40:41 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['your_database_name']
 collection = db['your_collection_name']
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
